334d3d2936520f2c62b60026cdb75def774da57d
LAVSEN DAHAL <lavsendahal@LAVSENs-MacBook-Pro.local>
2018-02-23 12:03:17 +0100

    Travis update

--- test_compute.py
+++ test_compute.py
@@ -1,9 +1,19 @@
 from compute import divide
 from compute import multiply
 
+@pytest.mark.parametrize(
+    'a ,b, x',
+    [(1, 2 , 0.5),
+    (4,2,2)]
+
+)
+
 def test_divide:
-    x=divide(1,2)
-    assert x==0.5
+    res=divide(1,2)
+    assert res==pytest.approx(x)
+
+
+
 
 
 def test_multiply:
